0a8e36b910066c2453165bbe51e32efa942033bd
ssa <webdev2027@gmail.com>
2019-04-25 09:32:32 +0200

    change favicons at headerdata

--- Configuration/TypoScript/Setup/014_headerdata.ts
+++ Configuration/TypoScript/Setup/014_headerdata.ts
@@ -3,22 +3,22 @@ page {
         // favicons at root
         199 = TEXT
         199.value (
-        <link rel="apple-touch-icon" sizes="57x57" href="typo3conf/ext/hellotypo3/Resources/Public/images/pbc/favicons/apple-icon-57x57.png">
-        <link rel="apple-touch-icon" sizes="60x60" href="typo3conf/ext/hellotypo3/Resources/Public/images/pbc/favicons/apple-icon-60x60.png">
-        <link rel="apple-touch-icon" sizes="72x72" href="typo3conf/ext/hellotypo3/Resources/Public/images/pbc/favicons/apple-icon-72x72.png">
-        <link rel="apple-touch-icon" sizes="76x76" href="typo3conf/ext/hellotypo3/Resources/Public/images/pbc/favicons/apple-icon-76x76.png">
-        <link rel="apple-touch-icon" sizes="114x114" href="typo3conf/ext/hellotypo3/Resources/Public/images/pbc/favicons/apple-icon-114x114.png">
-        <link rel="apple-touch-icon" sizes="120x120" href="typo3conf/ext/hellotypo3/Resources/Public/images/pbc/favicons/apple-icon-120x120.png">
-        <link rel="apple-touch-icon" sizes="144x144" href="typo3conf/ext/hellotypo3/Resources/Public/images/pbc/favicons/apple-icon-144x144.png">
-        <link rel="apple-touch-icon" sizes="152x152" href="typo3conf/ext/hellotypo3/Resources/Public/images/pbc/favicons/apple-icon-152x152.png">
-        <link rel="apple-touch-icon" sizes="180x180" href="typo3conf/ext/hellotypo3/Resources/Public/images/pbc/favicons/apple-icon-180x180.png">
-        <link rel="icon" type="image/png" sizes="192x192"  href="typo3conf/ext/hellotypo3/Resources/Public/images/pbc/favicons/android-icon-192x192.png">
-        <link rel="icon" type="image/png" sizes="32x32" href="typo3conf/ext/hellotypo3/Resources/Public/images/pbc/favicons/favicon-32x32.png">
-        <link rel="icon" type="image/png" sizes="96x96" href="typo3conf/ext/hellotypo3/Resources/Public/images/pbc/favicons/favicon-96x96.png">
-        <link rel="icon" type="image/png" sizes="16x16" href="typo3conf/ext/hellotypo3/Resources/Public/images/pbc/favicons/favicon-16x16.png">
-        <link rel="manifest" href="typo3conf/ext/hellotypo3/Resources/Public/images/pbc/favicons/manifest.json">
+        <link rel="apple-touch-icon" sizes="57x57" href="typo3conf/ext/hellotypo3/Resources/Public/images/favicons/apple-icon-57x57.png">
+        <link rel="apple-touch-icon" sizes="60x60" href="typo3conf/ext/hellotypo3/Resources/Public/images/favicons/apple-icon-60x60.png">
+        <link rel="apple-touch-icon" sizes="72x72" href="typo3conf/ext/hellotypo3/Resources/Public/images/favicons/apple-icon-72x72.png">
+        <link rel="apple-touch-icon" sizes="76x76" href="typo3conf/ext/hellotypo3/Resources/Public/images/favicons/apple-icon-76x76.png">
+        <link rel="apple-touch-icon" sizes="114x114" href="typo3conf/ext/hellotypo3/Resources/Public/images/favicons/apple-icon-114x114.png">
+        <link rel="apple-touch-icon" sizes="120x120" href="typo3conf/ext/hellotypo3/Resources/Public/images/favicons/apple-icon-120x120.png">
+        <link rel="apple-touch-icon" sizes="144x144" href="typo3conf/ext/hellotypo3/Resources/Public/images/favicons/apple-icon-144x144.png">
+        <link rel="apple-touch-icon" sizes="152x152" href="typo3conf/ext/hellotypo3/Resources/Public/images/favicons/apple-icon-152x152.png">
+        <link rel="apple-touch-icon" sizes="180x180" href="typo3conf/ext/hellotypo3/Resources/Public/images/favicons/apple-icon-180x180.png">
+        <link rel="icon" type="image/png" sizes="192x192"  href="typo3conf/ext/hellotypo3/Resources/Public/images/favicons/android-icon-192x192.png">
+        <link rel="icon" type="image/png" sizes="32x32" href="typo3conf/ext/hellotypo3/Resources/Public/images/favicons/favicon-32x32.png">
+        <link rel="icon" type="image/png" sizes="96x96" href="typo3conf/ext/hellotypo3/Resources/Public/images/favicons/favicon-96x96.png">
+        <link rel="icon" type="image/png" sizes="16x16" href="typo3conf/ext/hellotypo3/Resources/Public/images/favicons/favicon-16x16.png">
+        <link rel="manifest" href="typo3conf/ext/hellotypo3/Resources/Public/images/favicons/manifest.json">
         <meta name="msapplication-TileColor" content="#ffffff">
-        <meta name="msapplication-TileImage" content="typo3conf/ext/hellotypo3/Resources/Public/images/pbc/favicons/ms-icon-144x144.png">
+        <meta name="msapplication-TileImage" content="typo3conf/ext/hellotypo3/Resources/Public/images/favicons/ms-icon-144x144.png">
         <meta name="theme-color" content="#ffffff">
         )
     }
