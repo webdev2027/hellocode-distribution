c99115ee8acd607ef29e77d1e0f2e2798b821b0c
ssa <webdev2027@gmail.com>
2019-12-07 13:24:22 +0100

    update composer.json

--- ext_emconf.php
+++ ext_emconf.php
@@ -23,7 +23,7 @@ $EM_CONF[$_EXTKEY] = [
     'version' => '1.0.0',
     'constraints' => [
         'depends' => [
-            'typo3' => '8.7-9.5.99',
+            'typo3' => '9.5.99',
         ],
         'conflicts' => [],
         'suggests' => [],
